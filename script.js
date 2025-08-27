257c7127b1965f07461228cf2d1241ab13047590
Haider Abbas <116145613+haidking@users.noreply.github.com>
2025-08-27 15:53:46 +0500

    Added messages and scoreboard for better understanding

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 let draw = 0;
+let round = 0;
 
 // 1 represents rock
 // 2 represents paper
@@ -8,6 +9,7 @@ let draw = 0;
 
 function getHumanChoice() {
     let userChoice = document.getElementById('userChoice').value;
+    let message = document.getElementById('gameMessage');
     userChoice = userChoice.toLowerCase();
     if (userChoice === 'rock') {
         return 1
@@ -31,8 +33,38 @@ function guess(){
 
 function playRound(userChoice, computerChoice) {
     let message = document.getElementById('gameMessage');
+
+    let pc_choice = '';
+    if (computerChoice === 1) {
+        pc_choice = 'Rock';
+    } else if (computerChoice === 2) {
+        pc_choice = 'Paper';
+    } else {
+        pc_choice = 'Scissor';
+    };
+
     if (userChoice === computerChoice) {
         message.textContent = "It's a draw. Try again.";
         draw++;
+        round++;
+    } else if (userChoice === 1 && computerChoice === 2 || userChoice === 2 && computerChoice === 3 || userChoice === 3 && computerChoice === 1) {
+        message.textContent = "You lose. The computer chose " + pc_choice;
+        computerScore++;
+        round++;
+    } else if (userChoice === 1 && computerChoice === 3 || userChoice === 2 && computerChoice === 1 || userChoice === 3 && computerChoice === 2) {
+        message.textContent = "You win. The computer chose " + pc_choice;
+        humanScore++;
+        round++;
     }
+
+    humanScoreUpdate = document.getElementById('humanScore');
+    humanScoreUpdate.textContent = humanScore;
+    computerScoreUpdate = document.getElementById('computerScore');
+    computerScoreUpdate.textContent = computerScore;
+    drawUpdate = document.getElementById('draw');
+    drawUpdate.textContent = draw;
+    roundUpdate = document.getElementById('round');
+    roundUpdate.textContent = round;
+
+
 }
